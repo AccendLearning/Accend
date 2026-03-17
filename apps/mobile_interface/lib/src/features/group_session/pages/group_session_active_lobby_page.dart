a37ca810c4f603485e646ed8f97e0dd99ae4191e
Matthew Fehr <mattfehr2004@gmail.com>
2026-03-16 17:00:16 -0700

    create quick active group session back button and popup

--- apps/mobile_interface/lib/src/features/group_session/pages/group_session_active_lobby_page.dart
+++ apps/mobile_interface/lib/src/features/group_session/pages/group_session_active_lobby_page.dart
@@ -1,34 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import '../widgets/quit_group_session_back_button.dart';
 import '../../../app/constants.dart';
-import '../../../common/widgets/primary_button.dart';
-import '../controllers/group_session_controller.dart';
-import '../widgets/widget1.dart';
-import '../../../app/routes.dart' as routes;
-import '../widgets/private_button.dart' as private_button;
-import '../../../common/widgets/bottom_nav_bar.dart' as bot_nav_bar;
 
 class GroupSessionActiveLobbyPage extends StatefulWidget {
   const GroupSessionActiveLobbyPage({super.key});
 
   @override
-  State<GroupSessionActiveLobbyPage> createState() => _GroupSessionActiveLobbyPageState();
+  State<GroupSessionActiveLobbyPage> createState() =>
+      _GroupSessionActiveLobbyPageState();
 }
 
-class _GroupSessionActiveLobbyPageState extends State<GroupSessionActiveLobbyPage> {
- 
-
+class _GroupSessionActiveLobbyPageState
+    extends State<GroupSessionActiveLobbyPage> {
   final _lobbyCode = TextEditingController();
 
-
+  @override
+  void dispose() {
+    _lobbyCode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    // final ctrl = context.watch<GroupSessionController>();
     final t = Theme.of(context);
-
-    int _selectedIndex = 1;
 
     return Scaffold(
       body: SafeArea(
@@ -41,20 +35,16 @@ class _GroupSessionActiveLobbyPageState extends State<GroupSessionActiveLobbyPag
                 children: [
                   Stack(
                     children: [
-                      IconButton(
-                        onPressed: () => Navigator.maybePop(context),
-                        icon: const Icon(Icons.arrow_back_ios_new_rounded),
-                      ),
-
+                      const QuitGroupSessionBackButton(),
                       Align(
                         alignment: Alignment.center,
                         child: Padding(
-                          padding: EdgeInsets.only(top:8),
+                          padding: const EdgeInsets.only(top: 8),
                           child: RichText(
                             text: TextSpan(
                               style: t.textTheme.headlineMedium,
-                              children: [
-                                const TextSpan(text: '[TEMP PAGE]'),
+                              children: const [
+                                TextSpan(text: '[TEMP PAGE]'),
                               ],
                             ),
                           ),
@@ -62,35 +52,13 @@ class _GroupSessionActiveLobbyPageState extends State<GroupSessionActiveLobbyPag
                       ),
                     ],
                   ),
-
                   const SizedBox(height: 10),
-
                   Divider(
                     color: AppColors.border,
                     thickness: 5,
                   ),
-
-                  Spacer(),
-                  // const SizedBox(height: 30),
-
-                  // Builder(
-                  //   builder: (_) {
-                  //     // if (ctrl.isLoading) {
-                  //     //   return const Center(child: CircularProgressIndicator());
-                  //     // }
-
-                  //     // return RichText(
-                  //     //   text: TextSpan(
-                  //     //     style: t.textTheme.headlineMedium,
-                  //     //     children: [
-                  //     //       const TextSpan(text: ctrl.privateLobby["lobby_id"]: String), // TOTALLY WRONG, BASICALLY PSEUDOCODE, BUT IDK WHAT TO DO HERE TO JUSTMAKE IT DISPLAY THE LOBBY CODE
-                  //     //     ],
-                  //     //   ),
-                  //     // );
-                  //   },
-                  // ),
-
-                  Spacer(),
+                  const Spacer(),
+                  const Spacer(),
                 ],
               ),
             ),
